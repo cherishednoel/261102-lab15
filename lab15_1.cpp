5fa8843cf8719223c91a6ffba95b40fe33dcf216
cherishednoel <panida.stp@icloud.com>
2023-01-17 15:17:52 +0700

    lab15_1.cpp completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i = 1; i < N; i++){
+		T temp = d[i];
+		int j = i;
+		while(j > 0 && d[j-1] < temp){
+			d[j] = d[j-1];
+			j--;
+		}
+		d[j] = temp;
+		cout << "Pass " << i << ":";
+		for(int k = 0; k < N; k++) cout << d[k] << " ";
+		cout << "\n";
+	}
 }
 
 int main(){
